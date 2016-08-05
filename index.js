9d80136a5e57425e72c360dad8d26bd8daccc310
Mayur Dhurpate <mayurdhurpate@gmail.com>
2016-08-05 12:39:56 +0530

    indexjs modified herokou port

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
+
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
@@ -22,6 +26,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
