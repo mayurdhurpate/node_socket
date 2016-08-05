c4dc2405a180dff8583049ed3472acb5b110f490
Mayur Dhurpate <mayurdhurpate@gmail.com>
2016-08-05 18:17:06 +0530

    phasor game added. duplicate connections corrected. folders modified

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var port = process.env.PORT || 3000;
 
-
+app.use(express.static('static'));
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('templates/index.html');
 });
 
-io.on('connection', function(socket){
+app.get('/game', function(req, res){
+  res.sendfile('templates/game.html');
+});
 
-  console.log('a user connected'+io.engine.clientsCount);
+io.on('connection', function(socket){  
   var clientIp = socket.request.connection.remoteAddress;
   io.emit('chat message', clientIp+" connected! No. of clients: "+ io.engine.clientsCount);
-  io.on('connection', function(socket){
-	  socket.on('chat message', function(msg){
-	    io.emit('chat message', msg);
-	  });
-	  socket.on('disconnect', function(){
-	    var clientIp = socket.request.connection.remoteAddress;
-        io.emit('chat message', clientIp+" disconnected! No. of clients: "+ io.engine.clientsCount);
-	  });
-  });
+	console.log(clientIp+" connected! No. of clients: "+ io.engine.clientsCount);
+
+  socket.on('chat message', function(msg){
+	  io.emit('chat message', msg);
+	});
+
+	socket.on('disconnect', function(){
+	var clientIp = socket.request.connection.remoteAddress;
+    io.emit('chat message', clientIp+" disconnected! No. of clients: "+ io.engine.clientsCount);
+    console.log(clientIp+" disconnected! No. of clients: "+ io.engine.clientsCount);
+	});
+ 
 });
 
 http.listen(port, function(){
